b602e4a94eb21ed351bea0ba3cde3c1273ab42cb
sridhar10464 <nsridhar10464@gmail.com>
2022-11-14 12:56:58 +0530

    react to node to atlas

--- src/global.js
+++ src/global.js
@@ -1,2 +1,3 @@
 // export const API = "https://6321626a82f8687273b0bd30.mockapi.io";
-export const API = "http://localhost:4000";
+// export const API = "http://localhost:4000";
+export const API = "https://b39wd-tamilnode.herokuapp.com";
